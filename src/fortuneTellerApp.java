eb5f391981b8e8d6026ff363434e991977730127
George Essilfie <Georgeessilfie91@hotmail.com>
2018-05-05 16:52:28 -0400

    prep work pt1

---
+++ src/fortuneTellerApp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class fortuneTellerApp {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your first name?");
+		String firstName = input.nextLine();
+		System.out.println("What is your last name?");
+		String lastName = input.nextLine();
+		System.out.println("How old are you?");
+		String age = input.nextLine();
+		
+		
+		
+		
+	
+		
+		
+		
+		
+
+	}
+
+}
