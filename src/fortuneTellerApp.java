6140289e079260f21c1da62e322425f8f603011c
George Essilfie <Georgeessilfie91@hotmail.com>
2018-05-06 17:03:53 -0400

    Firstloop complete

--- src/fortuneTellerApp.java
+++ src/fortuneTellerApp.java
@@ -6,11 +6,42 @@ public class fortuneTellerApp {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("What is your first name?");
+	
 		String firstName = input.nextLine();
+		
+		System.out.println("Hello," + firstName);
+		
+		
 		System.out.println("What is your last name?");
+		
+		
 		String lastName = input.nextLine();
-		System.out.println("How old are you?");
-		String age = input.nextLine();
+		System.out.println("Thank you " + firstName + " " + lastName);
+		
+			
+		int number = 0;
+		boolean isNumberAge;
+				
+	
+		do {
+			System.out.println("How old are you?");
+			if (input.hasNextInt()) {
+				number = input.nextInt();
+				isNumberAge = true;
+			}else {
+				System.out.println("Please enter an integer for Age");
+				isNumberAge = false;
+				input.next();
+				
+							} 
+		} while (!(isNumberAge));		
+		
+		System.out.println("What is your birth month?");
+		int userMonth = input.nextInt();
+		
+
+		
+		
 		
 		
 		
